d16e1cacbcb68dae9234097098d805fc0c17e26d
Uzondu Stephen <stev.monye04@gmail.com>
2023-06-06 19:58:38 +0200

    last commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+ // Hide Results
+ document.getElementById('results').style.display = 'none';
+
+ // Show loader
+ document.getElementById('loading').style.display = 'block';
+
+ setTimeout(calculateResults, 1000);
+
+ e.preventDefault();
+});
 
 // Calculate Results
 function calculateResults(e){
@@ -24,15 +34,27 @@ if(isFinite(monthly)){
  monthlyPayment.value = monthly.toFixed(2);
  totalPayment.value = (monthly * calculatedPayments).toFixed(2);
  totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+ // Show results
+ document.getElementById('results').style.display = 'block';
+
+ // Hide Loader
+ document.getElementById('loading').style.display = 'none';
+
 } else {
  showError('Please fill all Fields');
 }
 
- e.preventDefault();
+ 
 }
 
 // Show Error
 function showError(error) {
+ // Hide results
+ document.getElementById('results').style.display = 'none';
+
+ // Hide Loader
+ document.getElementById('loading').style.display = 'none';
  // Create a div
  const errorDiv = document.createElement('div');
 
